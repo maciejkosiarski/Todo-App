82bae9f0b179f8b098cc42436bf6bf634bee0080
Maciej Kosiarski <maciej.kosiarski@pollster.pl>
2017-03-03 14:53:48 +0100

    app_example.js updated

--- config/app_example.js
+++ config/app_example.js
@@ -1,16 +1,32 @@
 module.exports = {
     'app': {
-        title:'',
-        port: 3000,
-        env: ''
+        title:'App Name',
+        port: 8080,
+        //production, development
+        env: 'production'
     },
     'security': {
-        cookie: '',
-        session: ''
+        salt: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        cookie: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        session: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     },
     'database': {
         'mongodb':{
-            url: ''
+            url: 'mongodb://user:pass@ds113000.mlab.com:13000/your-app',
+            'options':{ 
+                server:{ 
+                    socketOptions: { 
+                        keepAlive: 300000, 
+                        connectTimeoutMS: 30000 
+                    } 
+                }, 
+                replset:{ 
+                    socketOptions: { 
+                        keepAlive: 300000, 
+                        connectTimeoutMS : 30000 
+                    } 
+                } 
+            }
         }
     }
 };
