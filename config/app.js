ef0157833b9a2c16a107fa888967a0e567f0dadd
maciejkosiarski <maciek.kosiarski@gmail.com>
2017-05-21 22:56:55 +0200

    'heroku'

---
+++ config/app.js
@@ -0,0 +1,42 @@
+module.exports = {
+    'app': {
+        title:'Todo App',
+        port: 8080,
+        //production, development
+        env: 'development'
+    },
+    'security': {
+        salt: 'bjmkh7hkg09iopJklgYJB6',
+        cookie: 'bjmkh7hkg09sdfiopJklgYsdfJB6',
+        session: 'bjmssdfkh7hkg09iopJsdfklgYJB6'
+    },
+    'logs': {
+        cron: '/logs/cron.log',
+        access: '/logs/access.log'
+    },
+    'database': {
+        'mongodb':{
+            url: 'mongodb://admin:ZAQ!2wsxCDE#@ds115110.mlab.com:15110/mcek-app',
+            'options':{ 
+                server:{ 
+                    socketOptions: { 
+                        keepAlive: 300000, 
+                        connectTimeoutMS: 30000 
+                    } 
+                }, 
+                replset:{ 
+                    socketOptions: { 
+                        keepAlive: 300000, 
+                        connectTimeoutMS : 30000 
+                    } 
+                } 
+            }
+        }
+    },
+    'priority': {
+        low: 1,
+        medium: 2,
+        high: 3
+    }
+};
+
