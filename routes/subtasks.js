15a2174000c8af29ec1ee71fd3c0e00c43e9e8af
maciejkosiarski <maciek.kosiarski@gmail.com>
2017-05-21 22:27:56 +0100

    remove subtask fix

--- routes/subtasks.js
+++ routes/subtasks.js
@@ -59,7 +59,7 @@ router.delete('/', uuidValid, function (req, res, next) {
         if (err) return next(err);
         Task.findById(subtask._task, function (err, task) {
             if (err) return next(err);
-            var place = task.subtasks.indexOf(subtak._id);
+            var place = task.subtasks.indexOf(subtask._id);
             task.subtakss.splice(place, 1);
             task.save();
             req.flash('info', '<div class="alert alert-success">Subtask '+subtask.name+' was successful removed.</div>');
